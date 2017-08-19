18c2ba755e68c57616e26d577e4439897daf357a
Jesse <jranon@users.noreply.github.com>
2017-08-18 17:37:35 -0700

    got hangman JSON post game working

--- src/app.js
+++ src/app.js
@@ -14,6 +14,13 @@ const readWords = () => {
   return contents.split('\n');
 };
 
+// Set new String prototype method
+String.prototype.splice = function(startIndex, charDelete, strInsert) {
+  const charArray = this.split('');
+  Array.prototype.splice.apply(charArray, arguments);
+  return charArray.join('');
+}
+
 // Get random word
 const getRandomWord = () => {
   const arrayOfWords = readWords();
@@ -21,36 +28,80 @@ const getRandomWord = () => {
 };
 
 // Keep track of random word;
-
 const finalWord = getRandomWord().toLowerCase();
+console.log(finalWord);
 
 let wordSoFar = '';
 
-const setWordSoFarToSymbols = () => {
-  if(wordSoFar.length !== finalWord.length) {
-    for (let i = 0; i < finalWord.length; i++) {
-      wordSoFar = wordSoFar.concat('-');
-    };
+const initiateHiddenWord = (x) => {
+  for (let i = 0; i < x.length - 1; i++) {
+    wordSoFar += '-';
   }
-};
+}
 
-const setWordSoFar = (letterGuess) => {
-  if(letterGuess.length === 1) {
-    for (let i = 0; i < finalWord.length; i++){
-      if(finalWord[i] === letterGuess) {
-        wordSoFar.splice(i, 0, letterGuess);
-      }
+initiateHiddenWord(finalWord);
+
+console.log(`${wordSoFar}, ${wordSoFar.length}`);
+
+// track guesses
+
+const successfulGuesses = [];
+const badGuesses = [];
+let guesses = [];
+let won = false;
+
+const processGuess = (x) => {
+  const resObj = { wordSoFar, guesses: guesses.length };
+  if (won) return { message: 'You won! Refresh the page to try another word' };
+  if (badGuesses.length === 5) {
+    resObj = { message: 'You lost! Refresh the page to try another word.' };
+    return resObj;
+  }
+  if (guesses.includes(x)) {
+    resObj.message = `You\'ve already tried ${x}`;
+    return resObj;
+  }
+  let wasgood = false;
+  for (let i = 0; i < finalWord.length - 1; i++) {
+    if (x === finalWord[i]) {
+      let newWordSoFar = wordSoFar.split('');
+      newWordSoFar[i] = x;
+      wordSoFar = newWordSoFar.join('');
+      console.log(wordSoFar);
+      wasgood = true;
     }
   }
-  return "Please guess using one letter";
+  if (wasgood) {
+    successfulGuesses.push(x);
+    if (checkWin(wordSoFar)) {
+      won = true;
+      return { message: 'You win!' };
+    }
+    resObj.wordSoFar = wordSoFar;
+    resObj.message = `${x} was a good guess.`
+  }
+  else {
+    badGuesses.push(x);
+    resObj.message = `${x} was a bad guess.`
+  }
+  guesses = successfulGuesses.concat(badGuesses);
+  resObj.guesses = guesses.length;
+  resObj.guessesLeft = 5 - badGuesses.length;
+  return resObj;
+};
+
+const checkWin = function(str) {
+  let isGood = true;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '-') isGood = false;
+  }
+  return isGood;
 }
 
 // TODO: your code to handle requests
 
 server.get('/', (req, res) => {
-  res.send(wordSoFar);
-  setWordSoFarToSymbols();
-  console.log(wordSoFar);
+  res.send({ message: 'Welcome!  '});
 });
 
 server.post('/guess', (req, res) => {
@@ -60,9 +111,18 @@ server.post('/guess', (req, res) => {
   // if that letter (req) is found in the finalWord
   // then we change wordSoFar to reflect that.
   // wordSoFar (splice i in the letter)
-  let letterGuess = req.body.data;
-  setWordSoFar(letterGuess);
-  res.send(finalWord);
+  // const letterGuess = req.body.data;
+  // setWordSoFar(letterGuess);
+  // res.send(finalWord);
+  const { letter } = req.body;
+  console.log(req.body);
+  if (!letter || letter.length !== 1) {
+    res.status(STATUS_USER_ERROR);
+    res.json({ error: 'We need a single letter from you!' });
+    return;
+  }
+  res.json(processGuess(letter));
 });
 
 server.listen(3000);
+console.log('Server up and running on port 3000');
